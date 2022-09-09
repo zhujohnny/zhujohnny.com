35afd19f4b11c69df62d8775930a483ffb10f1b0
Johnny Zhu <zhujohnny@gmail.com>
2022-09-09 14:28:54 -0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,13 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  defaultMenuCollapsed: true,
+  github: 'https://github.com/zhujohnny',
+  docsRepositoryBase: 'https://github.com/zhujohnny/zhujohnny.com',
+  titleSuffix: ' – Johnny Zhu',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Johnny Zhu</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Los Angeles, CA
       </span>
     </>
   ),
